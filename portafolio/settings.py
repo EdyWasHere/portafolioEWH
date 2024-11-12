ea4e6e7ef1d65f9926c2cfa8814f383539a2bb3f
EdyWasHere <edmundomacielmtz97@gmail.com>
2024-11-11 23:07:35 -0600

    configuracion de websocket

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web'
+    'web',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'portafolio.wsgi.application'
+ASGI_APPLICATION = 'portafolio.asgi.application'
 
 
 # Database
