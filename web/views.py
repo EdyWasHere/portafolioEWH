91f91d1a05fd2f4af789f0fd5e63dc7d754db52b
EdyWasHere <edmundomacielmtz97@gmail.com>
2024-11-12 09:06:06 -0600

    cambio minimo

--- web/views.py
+++ web/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("¡Bienvenido a mi portafolio!")
+    return HttpResponse("¡Bienvenido a mi portafolio EWH!")
